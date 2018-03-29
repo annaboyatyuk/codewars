efe06eab7c22e8ca9b093bb60db4e3d8eae32d6d
annaboyatyuk <annaboyatyuk@gmail.com>
2018-03-28 20:56:00 -0700

    nice array

---
+++ niceArray/solution.js
@@ -0,0 +1,16 @@
+let tempArr = [];
+function isNice(arr){
+    if(!arr.length) {
+      return false;
+    };
+    tempArr = arr;
+    return tempArr.every(checkVal);
+};
+function checkVal(currentVal){
+    for (var i = 0; i < tempArr.length; i++){
+        if(currentVal + 1 === tempArr[i] || currentVal - 1 === tempArr[i]){
+            return true;
+        };
+    };
+};
+
